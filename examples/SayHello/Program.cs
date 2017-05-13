616227d1bf925dd08d28005b9df17f862dce288b
Alastair Smith <github@alastairsmith.me.uk>
2017-05-13 09:27:15 +0300

    Initialise project

---
+++ examples/SayHello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Falcon.Examples.SayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
